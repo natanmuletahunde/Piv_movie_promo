226b600fd9c30cef4f998a81de5d67eb6232c520
pivnati <natanmuleta911@gmail.com>
2024-09-05 18:34:45 -0700

    modify the contact  page  which is found inside the pages folder

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ContactPage from '../components/ContactPage';
+
+const Contact = () => {
+  return <ContactPage />;
+};
+
+export default Contact;

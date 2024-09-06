5422ccfdc55a01cdfdb210d9e7303a2ac970bce6
pivnati <natanmuleta911@gmail.com>
2024-09-05 21:17:38 -0700

    modify the the bout movies page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import NewMoviePost from '../components/NewMoviePost';
 
 const Movies = () => {
   return (
-    <section className="container mx-auto p-8 bg-black">
-      <h2 className="text-3xl font-bold text-white text-center mb-4">Latest Movies</h2>
-     
+    <section className="container mx-auto h-screen bg-black flex items-center justify-center">
+      <h2 className="text-3xl font-bold text-white text-center">Latest Movies</h2>
     </section>
   );
 };

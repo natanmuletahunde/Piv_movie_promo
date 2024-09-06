8fc94a05b293f5880ceca6221eddc2715ea0634f
pivnati <natanmuleta911@gmail.com>
2024-09-05 18:31:07 -0700

    modify the footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-center text-white">
+      <p>&copy; 2024 Movie Promotion Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;

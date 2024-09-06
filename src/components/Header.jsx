752b17f3bb7dea38923dd2df168c363dd66a1577
pivnati <natanmuleta911@gmail.com>
2024-09-05 20:33:15 -0700

    modify the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 p-4 text-white">
-      <div className="container mx-auto flex justify-between">
-        <h1 className="text-xl font-bold">Movie Promo</h1>
+    <header className="bg-gray-800 p-6 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Movie Promo</h1>
         <nav>
-          <Link to="/" className="px-3">Home</Link>
-          <Link to="/movies" className="px-3">Movies</Link>
-          <Link to="/about" className="px-3">About Us</Link>
-          <Link to="/contact" className="px-3">Contact</Link>
+          <NavLink
+            to="/"
+            className="px-4 hover:text-red-500 focus:text-red-600 active:text-red-600 font-poppins"
+            activeClassName="text-red-600"
+            exact
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/movies"
+            className="px-4 hover:text-red-500 focus:text-red-600 active:text-red-600 font-poppins"
+            activeClassName="text-red-600"
+          >
+            Movies
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="px-4 hover:text-red-500 focus:text-red-600 active:text-red-600 font-poppins"
+            activeClassName="text-red-600"
+          >
+            About Us
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="px-4 hover:text-red-500 focus:text-red-600 active:text-red-600 font-poppins"
+            activeClassName="text-red-600"
+          >
+            Contact
+          </NavLink>
         </nav>
       </div>
     </header>

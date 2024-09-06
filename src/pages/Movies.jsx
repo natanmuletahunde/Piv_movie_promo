b3a51a97d8f8a9407dd3388cbcdcb7a7f41197b6
pivnati <natanmuleta911@gmail.com>
2024-09-05 21:19:39 -0700

    modify the movies page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 const Movies = () => {
   return (
-    <section className="container mx-auto h-screen bg-black flex items-center justify-center">
-      <h2 className="text-3xl font-bold text-white text-center">Latest Movies</h2>
+    <section className="container mx-auto h-screen bg-black flex flex-col items-center">
+      <h2 className="text-6xl font-bold text-white text-center mt-8">Latest Movies</h2>
+      <div className="flex-grow"></div> {/* This will push the content to the top */}
     </section>
   );
 };

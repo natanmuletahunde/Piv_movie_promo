04f8007cb9c65716b4fc8b5e07eb1e01d806fc1d
pivnati <natanmuleta911@gmail.com>
2024-09-05 18:32:30 -0700

    modify the aboutuspage

---
+++ src/components/AboutUsPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <section className="container mx-auto p-8">
+      <h2 className="text-3xl font-bold mb-4">About Us</h2>
+      <p>We are dedicated to promoting the best movies. Stay updated with the latest movie news and releases.</p>
+    </section>
+  );
+};
+
+export default AboutUsPage;
